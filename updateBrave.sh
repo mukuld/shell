3ccfcd8a9a41b9609c4340936546a24f81d4e206
Mukul Dharwadkar <dharmuku@amazon.com>
2020-04-10 22:46:20 -0700

    Brave Script

---
+++ updateBrave.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo cp ~/Documents/temp/parameterized-app.sh /Applications/Brave\ Browser.app/Contents/MacOS/
+sudo cp ~/Documents/temp/Info.plist /Applications/Brave\ Browser.app/Contents/
+
+sudo mv /Applications/Brave\ Browser.app/ ~/Documents/temp/
+#echo "Sleeping for 5 seconds"
+#sleep 5
+#sudo mv ~/Documents/temp/Brave\ Browser.app/ /Applications/
