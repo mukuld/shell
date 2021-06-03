9b138f416d094867d9974df924e015fb0329c6c6
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2021-06-03 22:37:02 +0000

    Monitoring script

--- gitpulldsp.sh
+++ gitpulldsp.sh
@@ -6,4 +6,4 @@ WORKDIR=$HOME/dilippandit
 LOGFILE1=gitpulldsp.log
 cd $WORKDIR
 echo $(date) >> $LOGDIR/$LOGFILE1
-sudo git pull >> $LOGDIR/$LOGFILE1
+git pull >> $LOGDIR/$LOGFILE1
---
+++ used-resources.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+LOGFILE=/home/ec2-user/data/logs/monitoring.log
+INST=$(/usr/bin/curl -s http://169.254.169.254/latest/meta-data/instance-id) > /dev/null 2>&1
+USEDMEMORY=$(/bin/free -m | awk 'NR==2{printf "%.2f\t", $3*100/$2 }')
+USEDDISK=$(/bin/df -m | awk 'NR==6{printf "%.1f\t", $5}')
+aws cloudwatch put-metric-data --metric-name memory-usage --dimensions Instance=$INST --namespace "Custom: Utilization" --value $USEDMEMORY
+aws cloudwatch put-metric-data --metric-name disk-usage --dimensions Instance=$INST --namespace "Custom: Utilization" --value $USEDDISK
