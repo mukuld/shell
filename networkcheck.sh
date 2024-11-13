747dbc358590bb5116085fbc2cd1a5d0e7c9d9c5
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2024-11-13 13:23:48 -0800

    Fixing issue

--- networkcheck.sh
+++ networkcheck.sh
@@ -10,17 +10,17 @@ LOGDIR=/home/mukul/Documents/logs/shell
 LOGFILE1=networkcheck.log
 CMD=$SYS_PATH/dig
 IFACE=wlp3s0
-PRINT_DATE=`echo $(date) >> $LOGDIR/$LOGFILE1`
-REPAIR_CMD=`/usr/sbin/ip link set $IFACE down; /usr/sbin/ip link set $IFACE up`
+#PRINT_DATE=`echo $(date) >> $LOGDIR/$LOGFILE1`
+REPAIR_CMD=$(/usr/sbin/ip link set $IFACE down; /usr/sbin/ip link set $IFACE up)
 
 NETWORKCHECK=`$CMD $HOST | grep -i $HOST | tail -1 | awk '{print $5}'`
 
 if [ -z "$NETWORKCHECK" ];
     then 
-        $PRINT_DATE
+        date >> $LOGDIR/$LOGFILE1
         $REPAIR_CMD >> $LOGDIR/$LOGFILE1
         echo "Restarted network to repair connectivity" >> $LOGDIR/$LOGFILE1
 else
-    $PRINT_DATE
+    date >> $LOGDIR/$LOGFILE1
     echo "All OK" >> $LOGDIR/$LOGFILE1
 fi
